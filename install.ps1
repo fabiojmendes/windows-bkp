e4405c86843b807864385f14b3181b58408667ae
Fabio Mendes <fabiojmendes@gmail.com>
2025-06-25 17:22:40 -0400

    Update installation script

--- install.ps1
+++ install.ps1
@@ -5,13 +5,17 @@ if (-not (Test-Path -Type Container -Path $install_path)) {
   New-Item -ItemType  Directory -Path $install_path
 }
 
-Copy-Item -Path backup.ps1 -Destination $install_path
-
 if (-not (Test-Path -Type Leaf -Path $install_path\config.ps1)) {
   Write-Host "First time installation, config file needs to be populated"
-  Copy-Item -Path config.sample.ps1 -Destination $install_path\config.ps1
-  notepad $install_path\config.ps1
+    try {
+      Copy-Item -Path config.ps1 -Destination $install_path\config.ps1
+    } catch {
+      Write-Error "Create a copy of config.sample.ps1 named config.ps1 before continuing with the installation"
+      exit 1
+    }
 }
+
+Copy-Item -Path backup.ps1 -Destination $install_path
 
 $backup_task_name = "BackupHome"
 $backup_task = Get-ScheduledTask $backup_task_name -ErrorAction SilentlyContinue
@@ -19,8 +23,8 @@ if(-not ($backup_task)) {
   try {
     Register-ScheduledTask -TaskName $backup_task_name -Xml $(Get-Content BackupHome.xml | Out-String) 
   } catch {
-    Write-Error "[[Scheduler]] Setting up backup task schedule failed: $_"
+    Write-Error "Setting up backup task schedule failed: $_"
   }
 } else {
-  Write-Warning "[[Scheduler]] Backup task not scheduled: there is already a task with the name '$backup_task_name'."
+  Write-Warning "Backup task not scheduled: there is already a task with the name '$backup_task_name'."
 }
